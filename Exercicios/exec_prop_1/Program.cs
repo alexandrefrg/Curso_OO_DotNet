294b184c5a64425dfd2793820e0ad9055574bcb3
Alexandre <alexandrefrg@hotmail.com>
2021-10-28 23:06:54 -0300

    sequential structure exercises created and started

---
+++ Exercicios/exec_prop_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exec_prop_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            int sum;
+
+            firstNumber = int.Parse(Console.ReadLine());
+            secondNumber = int.Parse(Console.ReadLine());
+
+            sum = firstNumber + secondNumber;
+            Console.WriteLine($"SOMA = {sum}");
+        }
+    }
+}
---
+++ Exercicios/exec_prop_1/exec_prop_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exec_prop_4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
