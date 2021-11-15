6de48afba75b5e702020a698397a04e3b3e39eba
Alexandre <alexandrefrg@hotmail.com>
2021-11-15 18:42:18 -0300

    created structure and add exercises instructions

---
+++ Exercicios/exercicios_proposto/exec_membros_estaticos/exec_mem_est_prop_1/exec_mem_est_prop_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exec_mem_est_prop_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
