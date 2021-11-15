d39a20f8623b7186b79f1dff05f0592c8e362e20
Alexandre <alexandrefrg@hotmail.com>
2021-11-14 21:43:21 -0300

    created structure for new exercises, and finished class exercises

---
+++ Exercicios/primeiros_exercicios/prim_exec_2/prim_exec_2/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace prim_exec_2
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ Exercicios/primeiros_exercicios/prim_exec_2/prim_exec_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace prim_exec_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Dados do primeiro funcionario:");
+            Funcionario f1 = new Funcionario();
+            Console.Write("Nome: ");
+            f1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionario:");
+            Funcionario f2 = new Funcionario();
+            Console.Write("Nome: ");
+            f2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            f2.Salario = double.Parse(Console.ReadLine());
+
+            double average_salary = (f1.Salario + f2.Salario) / 2.0;
+
+            Console.WriteLine($"Salário médio = {average_salary:F2}");
+        }
+    }
+}
