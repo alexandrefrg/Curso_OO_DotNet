20f9e3e7f0dfe629a83dba569b0d673fc2212537
Alexandre <alexandrefrg@hotmail.com>
2022-10-02 00:52:17 -0300

    chore: including cap6 exercices

---
+++ Exercicios/exercicios_fixacao/capitulo_6/cap6_exec_fix_1/Estudante.cs
@@ -0,0 +1,21 @@
+﻿namespace cap6_exec_fix_1
+{
+    public class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {Email}";
+        }
+
+    }
+}
+
---
+++ Exercicios/exercicios_fixacao/capitulo_6/cap6_exec_fix_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace cap6_exec_fix_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int qtdeAlugueis = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qtdeAlugueis; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}");
+                Console.Write("Nome: ");
+                String nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                String email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int nroQuarto = int.Parse(Console.ReadLine());
+
+                vect[nroQuarto] = new Estudante(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                if (vect[i] != null)
+                    Console.WriteLine($"{i}: {vect[i]}");
+            }
+        }
+    }
+}
+
